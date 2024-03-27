0bb2f26d1fd72f6bf1de50f0fa308b7fee715fa4
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-03-27 19:21:56 -0300

    Create CalculoDaAreaTerreno.py
    CALCULANDO A ÁREA DE UM TERRENO COM FUNÇÕES: DEF ( )

---
+++ CalculoDaAreaTerreno.py
@@ -0,0 +1,19 @@
+def mostralinha():
+    print('-'*40)
+mostralinha()
+print('{:^40}'.format('  CÁLCULOS DA ÁREA DE TERRENOS '))    
+mostralinha()
+
+def terreno(largura,comprimento):
+    soma = largura * comprimento
+    mostralinha()
+    print('{:^40}'.format(' RESULTADO DO CÁLCULO: '))
+    print(f'A área de um terreno de {largura} x {comprimento} é de {soma} m².') 
+
+
+largura = float(input('Largura(m):  '))
+comprimento = float(input('Comprimento(m): '))
+terreno(largura, comprimento)
+mostralinha()    
+
+            
