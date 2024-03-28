2b1f80b6634eb829bcee69de6337e80661578563
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-03-27 22:38:20 -0300

    print
    Sistema de Mensagens

---
+++ Print.py
@@ -0,0 +1,15 @@
+def mostralinha():
+    print('-'*36)
+mostralinha()    
+print('     SISTEMA DE MENSAGENS    ')
+mostralinha()
+
+def escreva(msg):
+    print(msg)
+    tam = len(msg) + 4
+    print('~'*tam)
+    print(msg)
+    print('~'*tam)
+
+escreva('Gustavo Guanabara')
+
